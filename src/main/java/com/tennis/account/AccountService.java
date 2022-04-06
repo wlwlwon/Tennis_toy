c4458ff0287a26e15e876663450ab7a25a403f1c
wlwlwon <ryanwon@naver.com>
2022-04-06 22:23:39 +0900

    Validator && CurrentAccount Add

--- src/main/java/com/tennis/account/AccountService.java
+++ src/main/java/com/tennis/account/AccountService.java
@@ -2,6 +2,9 @@ package com.tennis.account;
 
 
 import com.tennis.account.form.SignUpForm;
+import com.tennis.config.AppProperties;
+import com.tennis.mail.EmailMessage;
+import com.tennis.mail.EmailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -14,6 +17,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
@@ -24,10 +29,13 @@ import java.util.List;
 public class AccountService implements UserDetailsService {
 
     private final AccountRepository accountRepository;
-    private final PasswordEncoder passwordEncoder;
     private final JavaMailSender javaMailSender;
+    private final PasswordEncoder passwordEncoder;
+    private final TemplateEngine templateEngine;
+    private final AppProperties appProperties;
+    private final EmailService emailService;
 
-
+//    private final AuthenticationManager authenticationManager;
 
     @Transactional
     public Account processNewAccount(SignUpForm signUpForm) {
@@ -36,16 +44,6 @@ public class AccountService implements UserDetailsService {
         sendSignUpConfirmEmail(newAccount);
         return newAccount;
     }
-
-    public void sendSignUpConfirmEmail(Account newAccount) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(newAccount.getEmail());
-        mailMessage.setSubject("회원 가입 인증");
-        mailMessage.setText("/check-email-token?token="+newAccount.getEmailCheckToken()
-                +"&email=" + newAccount.getEmail());
-        javaMailSender.send(mailMessage);
-    }
-
     private Account saveNewAccount(@Valid SignUpForm signUpForm) {
         Account account = Account.builder()
                 .email(signUpForm.getEmail())
@@ -56,6 +54,15 @@ public class AccountService implements UserDetailsService {
                 .groupUpdatedByWeb(true)
                 .build();
         return accountRepository.save(account);
+    }
+
+    public void sendSignUpConfirmEmail(Account newAccount) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(newAccount.getEmail());
+        mailMessage.setSubject("회원 가입 인증");
+        mailMessage.setText("/check-email-token?token="+newAccount.getEmailCheckToken()
+                +"&email=" + newAccount.getEmail());
+        javaMailSender.send(mailMessage);
     }
 
     public void login(Account account) {
@@ -80,5 +87,25 @@ public class AccountService implements UserDetailsService {
         }
 
         return new UserAccount(account);
+    }
+
+    public void sendLoginLink(Account account) {
+
+        Context context = new Context();
+        context.setVariable("link","/login-by-email?token=" + account.getEmailCheckToken() +
+                "&email=" + account.getEmail());
+        context.setVariable("nickName",account.getNickname());
+        context.setVariable("linkName","이메일로 로그인하기");
+        context.setVariable("message","로그인하려면 아래 링크를 클릭하세요.");
+        context.setVariable("host",appProperties.getHost());
+        String message = templateEngine.process("mail/simple-link", context);
+
+        EmailMessage emailMessage = EmailMessage.builder()
+                .to(account.getEmail())
+                .subject("니스테?, 로그인 링크")
+                .message(message)
+                .build();
+
+        emailService.sendEmail(emailMessage);
     }
 }
---
+++ src/main/java/com/tennis/account/validator/SignUpFormValidator.java
@@ -0,0 +1,33 @@
+package com.tennis.account.validator;
+
+
+import com.tennis.account.AccountRepository;
+import com.tennis.account.form.SignUpForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@RequiredArgsConstructor
+public class SignUpFormValidator implements Validator {
+
+    private final AccountRepository accountRepository;
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.isAssignableFrom(SignUpForm.class);
+    }
+
+
+    @Override
+    public void validate(Object object, Errors errors) {
+        SignUpForm signUpForm = (SignUpForm)object;
+        if (accountRepository.existsByEmail(signUpForm.getEmail())) {
+            errors.rejectValue("email", "invalid.email", new Object[]{signUpForm.getEmail()}, "이미 사용중인 이메일입니다.");
+        }
+
+        if (accountRepository.existsByNickname(signUpForm.getNickname())) {
+            errors.rejectValue("nickname", "invalid.nickname", new Object[]{signUpForm.getEmail()}, "이미 사용중인 닉네임입니다.");
+        }
+    }
+}
