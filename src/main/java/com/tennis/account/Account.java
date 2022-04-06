59484c1b543d0442e95b580f5d7fcf1746cac5ef
wlwlwon <ryanwon@naver.com>
2022-04-06 21:28:02 +0900

    Account && SecurityConfig add

---
+++ src/main/java/com/tennis/account/Account.java
@@ -0,0 +1,76 @@
+package com.tennis.account;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime emailCheckTokenGeneratedAt;
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    @Lob
+    @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean groupCreatedByEmail;
+
+    private boolean groupCreatedByWeb = true;
+
+    private boolean groupEnrollmentResultByEmail;
+
+    private boolean groupEnrollmentResultByWeb = true;
+
+    private boolean groupUpdatedByEmail;
+
+    private boolean groupUpdatedByWeb = true;
+
+    public void generateEmailCheckToken() {
+        this.emailCheckToken = UUID.randomUUID().toString();
+        this.emailCheckTokenGeneratedAt = LocalDateTime.now();
+    }
+
+    public void completeSignUp() {
+        this.emailVerified = true;
+        this.joinedAt = LocalDateTime.now();
+    }
+
+    public boolean isValidToken(String token) {
+        return this.emailCheckToken.equals(token);
+    }
+
+    public boolean canSendConfirmEmail() {
+        return this.emailCheckTokenGeneratedAt.isBefore(LocalDateTime.now().minusHours(1));
+    }
+}
